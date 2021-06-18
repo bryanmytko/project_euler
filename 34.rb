890216f1b4a0f364c7d9bdcf67a63ff16b150920
Bryan Mytko <bryanmytko@gmail.com>
2021-06-18 05:05:44 -0400

    Add cachine and Integer#to_a monkey patch

--- 34.rb
+++ 34.rb
@@ -4,9 +4,26 @@
 #
 # Note: As 1! = 1 and 2! = 2 are not sums they are not included.
 
-# 2540161 upper limit  9!7 because 9! * 8 is too big for any solution
+# 2540161 works as an upper limit
+# 9! * 7 = 7 digits vs 7 digit number 9999999
+# 9! * 8 = 7 digits vs 8 digit number 99999999
 
 require 'benchmark'
+
+# found this online: https://coderwall.com/p/h2ixcg/converting-integer-to-array-with-ruby
+# seems to be a bit faster than converting to a string and splitting
+# real: 5.355145 -> real: 2.705866 
+class Integer
+  def to_a
+    arr = []
+    tmp = self
+    while tmp>0
+      arr << tmp%10
+      tmp /= 10
+    end
+    arr.reverse
+  end
+end
 
 found = []
 cache = {}
@@ -15,7 +32,8 @@ cache = {}
 #   x.report("factorial") do
 
 (3..2540161).each do |d|
-  digits = d.to_s.split('').map(&:to_i) # this part is slow. takes around 3.5s on MBP
+  digits = d.to_a # see monkey patch above
+  # digits = d.to_s.split('').map(&:to_i) # this part is slow. takes around 3.5s on MBP
 
   # simple caching the factorials cuts time in half
   factorials = digits.map do |digit|
