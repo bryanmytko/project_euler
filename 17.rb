c11647d4fa959af2f69b9cd713bbaf122b97f6e4
Bryan Mytko <bryanmytko@gmail.com>
2021-06-24 03:50:50 -0400

    Add refactor

--- 17.rb
+++ 17.rb
@@ -75,3 +75,98 @@ p = (1..1000).inject("") do |t,i|
 end
 
 print p.size
+
+# Refactor attempt:
+#
+# If the numbers 1 to 5 are written out in words:
+# one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+#
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words,
+# how many letters would be used?
+#
+# NOTE: Do not count spaces or hyphens.
+# For example, 342 (three hundred and forty-two) contains 23 letters
+# and 115 (one hundred and fifteen) contains 20 letters.
+#
+# The use of "and" when writing out numbers is in compliance with British usage.
+# require "rspec"
+#
+# total = 0
+#
+# def int_to_word(n)
+#   i2w = {
+#     0 => nil,
+#     1 =>  "one",
+#     2 =>  "two",
+#     3 =>  "three",
+#     4 =>  "four",
+#     5 =>  "five",
+#     6 =>  "six",
+#     7 =>  "seven",
+#     8 =>  "eight",
+#     9 =>  "nine",
+#     10 => "ten",
+#     11 => "eleven",
+#     12 => "twelve",
+#     13 => "thirteen",
+#     14 => "fourteen",
+#     15 => "fifteen",
+#     16 => "sixteen",
+#     17 => "seventeen",
+#     18 => "eighteen",
+#     19 => "nineteen",
+#     20 => "twenty",
+#     30 => "thirty",
+#     40 => "forty",
+#     50 => "fifty",
+#     60 => "sixty",
+#     70 => "seventy",
+#     80 => "eighty",
+#     90 => "ninety",
+#     100 => "one hundred and",
+#     200 => "two hundred and",
+#     300 => "three hundred and",
+#     400 => "four hundred and",
+#     500 => "five hundred and",
+#     600 => "six hundred and",
+#     700 => "seven hundred and",
+#     800 => "eight hundred and",
+#     900 => "nine hundred and",
+#     1000 => "one thousand",
+#   }
+#
+#   p [i2w[n/100*100], i2w[n%100/10*10], i2w[n%10]]
+#   [i2w[n/100*100], i2w[n%100/10*10], i2w[n%10]].compact.join(" ")
+# end
+#
+# (1..1000).each do |n|
+#   total += int_to_word(n).gsub(/[^a-z]/, "").length
+# end
+#
+# print "Answer: #{total}"
+#
+# RSpec.describe "int to words" do
+#   it "converts 1000" do
+#     n = 1000
+#
+#     expect(int_to_word(n)).to eql("one thousand")
+#   end
+#
+#   it "converts a three digit number" do
+#     n = 123
+#
+#     expect(int_to_word(n)).to eql("one hundred and twenty three")
+#   end
+#
+#   it "converts a two digit nunber" do
+#     n = 89
+#
+#     expect(int_to_word(n)).to eql("eighty nine")
+#   end
+#
+#   it "converts a single digit number" do
+#     n = 7
+#
+#     expect(int_to_word(n)).to eql("seven")
+#   end
+# end
